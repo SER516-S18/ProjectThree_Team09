87267787e44f2bef7d438b1e787b7ddb375137a7
Nelson Tran <nutty@wha.al>
2018-03-27 14:30:24 -0700

    Add MessageDecoder and MessageEncoder

---
+++ src/main/java/ser516/project3/model/MessageDecoder.java
@@ -0,0 +1,63 @@
+package ser516.project3.model;
+
+import ser516.project3.model.Message;
+
+import java.io.StringReader;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.websocket.DecodeException;
+import javax.websocket.Decoder;
+import javax.websocket.EndpointConfig;
+
+public class MessageDecoder implements Decoder.Text<Message> {
+
+    @Override
+    public void init(EndpointConfig config) {
+        // Intentionally empty.
+    }
+
+    @Override
+    public void destroy() {
+        // Intentionally empty.
+    }
+
+    @Override
+    public Message decode(String payload) throws DecodeException {
+
+        Message message = new Message();
+
+        // Read the payload.
+        JsonObject root = Json.createReader(new StringReader(payload)).readObject();
+        JsonObject expressiveAttributes = root.getJsonObject("Expressive");
+        JsonObject affectiveAttributes = root.getJsonObject("Affective");
+
+        // Unmarshal the expressive attributes.
+        message.setLookingRight(expressiveAttributes.getBoolean("LookingRight"));
+        message.setLookingLeft(expressiveAttributes.getBoolean("LookingLeft"));
+        message.setLookingDown(expressiveAttributes.getBoolean("LookingDown"));
+        message.setLookingUp(expressiveAttributes.getBoolean("LookingUp"));
+        message.setEyebrowRaise(expressiveAttributes.getBoolean("EyebrowRaise"));
+        message.setRightWink(expressiveAttributes.getBoolean("RightWink"));
+        message.setLeftWink(expressiveAttributes.getBoolean("LeftWink"));
+        message.setBlink(expressiveAttributes.getBoolean("Blink"));
+        message.setEyesOpen(expressiveAttributes.getBoolean("EyesOpen"));
+        message.setSmile(expressiveAttributes.getBoolean("Smile"));
+        message.setClench(expressiveAttributes.getBoolean("Clench"));
+
+        // Unmarshal the affective attributes.
+        message.setMeditation(affectiveAttributes.getJsonNumber("Meditation").doubleValue());
+        message.setFrustration(affectiveAttributes.getJsonNumber("Frustration").doubleValue());
+        message.setEngagementBoredom(affectiveAttributes.getJsonNumber("EngagementBoredom").doubleValue());
+        message.setExcitementShortTerm(affectiveAttributes.getJsonNumber("ExcitementShortTerm").doubleValue());
+        message.setExcitementLongTerm(affectiveAttributes.getJsonNumber("ExcitementLongTerm").doubleValue());
+
+        return message;
+    }
+
+    @Override
+    public boolean willDecode(String s) {
+        return true;
+    }
+
+}
---
+++ src/main/java/ser516/project3/model/MessageEncoder.java
@@ -0,0 +1,66 @@
+package ser516.project3.model;
+
+import ser516.project3.model.Message;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonBuilderFactory;
+import javax.websocket.EncodeException;
+import javax.websocket.Encoder;
+import javax.websocket.EndpointConfig;
+
+public class MessageEncoder implements Encoder.Text<Message> {
+
+    @Override
+    public void init(EndpointConfig config) {
+        // Intentionally empty.
+    }
+
+    @Override
+    public void destroy() {
+        // Intentionally empty.
+    }
+
+    @Override
+    public String encode(Message message) throws EncodeException {
+        Map<String, Object> config = new HashMap<String, Object>();
+        JsonBuilderFactory factory = Json.createBuilderFactory(config);
+        
+        // Build "Expressive" object.
+        JsonObject expressiveObject = factory
+            .createObjectBuilder()
+            .add("LookingRight", message.getLookingRight())
+            .add("LookingLeft", message.getLookingLeft())
+            .add("LookingDown", message.getLookingDown())
+            .add("LookingUp", message.getLookingUp())
+            .add("EyebrowRaise", message.getEyebrowRaise())
+            .add("RightWink", message.getRightWink())
+            .add("LeftWink", message.getLeftWink())
+            .add("Blink", message.getBlink())
+            .add("EyesOpen", message.getEyesOpen())
+            .add("Smile", message.getSmile())
+            .add("Clench", message.getClench())
+            .build();
+        
+        // Build "Affective" object.
+        JsonObject affectiveObject = factory
+            .createObjectBuilder()
+            .add("Meditation", message.getMeditation())
+            .add("Frustration", message.getFrustration())
+            .add("EngagementBoredom", message.getEngagementBoredom())
+            .add("ExcitementShortTerm", message.getExcitementShortTerm())
+            .add("ExcitementLongTerm", message.getExcitementLongTerm())
+            .build();
+
+        return Json
+            .createObjectBuilder()
+            .add("Expressive", expressiveObject)
+            .add("Affective", affectiveObject)
+            .build()
+            .toString();
+    }
+
+}
