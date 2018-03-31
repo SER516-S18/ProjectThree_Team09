77fca209aa1c268d9e2ea72004f5480939624f8a
Sangeetha <sangeetha151294@gmail.com>
2018-03-31 00:34:34 -0700

    1. Generic Layout for Configuration Panel is added. 2. Expression Panel Compounds are added

--- src/main/java/ser516/project3/server/view/ServerPanelGenerator.java
+++ src/main/java/ser516/project3/server/view/ServerPanelGenerator.java
@@ -6,14 +6,22 @@ import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import javax.swing.AbstractButton;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSplitPane;
 import javax.swing.JTextField;
+import javax.swing.JToggleButton;
 import javax.swing.SwingConstants;
 import javax.swing.border.Border;
 import javax.swing.border.TitledBorder;
@@ -22,10 +30,12 @@ import javax.swing.event.DocumentListener;
 
 import ser516.project3.utilities.InputVerifierNumericals;
 import ser516.project3.utilities.ServerCommonData;
+import java.awt.BorderLayout;
 
 public class ServerPanelGenerator {
 
 	private static final Font FONT = new Font("Courier New", Font.BOLD, 17);
+	private static final Font SUBFONT = new Font("Courier New", Font.BOLD, 14);
 
 	/**
 	 * This method will initialize the top JPanels of the server application
@@ -59,12 +69,12 @@ public class ServerPanelGenerator {
 
 			@Override
 			public void removeUpdate(DocumentEvent e) {
-//				updateIntervalInputTextField(intervalInputTextField);
+				// updateIntervalInputTextField(intervalInputTextField);
 			}
 
 			@Override
 			public void insertUpdate(DocumentEvent e) {
-//				updateIntervalInputTextField(intervalInputTextField);
+				// updateIntervalInputTextField(intervalInputTextField);
 			}
 
 			@Override
@@ -124,9 +134,23 @@ public class ServerPanelGenerator {
 	 * This method will initialize the second sub panel of the Server window
 	 * 
 	 * @return the second sub-panel
+	 * @wbp.parser.entryPoint
 	 */
 	public static Component createConfigurationPanels() {
 		JPanel configPanel = new JPanel();
+		JPanel timerPanel = new JPanel();
+		JPanel emotionsPanel = new JPanel();
+		JPanel exprPanel = new JPanel();
+		JPanel consolePanel = new JPanel();
+
+		JSplitPane splitPane1 = new JSplitPane();
+		JSplitPane splitPane2 = new JSplitPane();
+		JSplitPane splitPane3 = new JSplitPane();
+
+		String[] lowerFaceList = { "Smile", "Clench", "Smirk Left", "Smirk Right", "Laugh" };
+		String[] upperFaceList = { "Raise Brow", "Furrow Brow" };
+		String[] eyeList = { "Blink", "Wink Left", "Wink Right", "Look Left", "Look Right" };
+
 		configPanel.setOpaque(false);
 		Border titledBorder = new TitledBorder(null, "Configuration", TitledBorder.LEADING, TitledBorder.TOP, FONT,
 				null);
@@ -135,9 +159,162 @@ public class ServerPanelGenerator {
 		Border compound = BorderFactory.createCompoundBorder(marginBorder, titledBorder);
 		configPanel.setBorder(compound);
 
-		configPanel.setLayout(new GridBagLayout());
-		new GridBagConstraints();
+		configPanel.setLayout(new BorderLayout(0, 0));
 
+		exprPanel.setLayout(new GridBagLayout());
+
+		emotionsPanel
+				.setBorder(new TitledBorder(null, "Emotions", TitledBorder.LEADING, TitledBorder.TOP, SUBFONT, null));
+		exprPanel.setBorder(
+				new TitledBorder(null, "Expressions", TitledBorder.LEADING, TitledBorder.TOP, SUBFONT, null));
+		consolePanel
+				.setBorder(new TitledBorder(null, "Console", TitledBorder.LEADING, TitledBorder.TOP, SUBFONT, null));
+
+		JLabel lowerFaceLbl = new JLabel("Lower Face");
+		GridBagConstraints lowerFaceGbc = new GridBagConstraints();
+		lowerFaceGbc.gridx = 1;
+		lowerFaceGbc.gridy = 1;
+		exprPanel.add(lowerFaceLbl, lowerFaceGbc);
+
+		JComboBox<Object> lowerFaceCombo = new JComboBox<Object>();
+		lowerFaceCombo.setModel(new DefaultComboBoxModel<Object>(lowerFaceList));
+		GridBagConstraints lowerFaceComboGbc = new GridBagConstraints();
+		lowerFaceComboGbc.gridx = 2;
+		lowerFaceComboGbc.gridy = 1;
+		exprPanel.add(lowerFaceCombo, lowerFaceComboGbc);
+
+		lowerFaceCombo.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				updateLowerFace(String.valueOf(lowerFaceCombo.getSelectedItem()));
+			}
+		});
+
+		JToggleButton lowerFaceAct = new JToggleButton("Activate");
+		GridBagConstraints lowerFaceActGbc = new GridBagConstraints();
+		lowerFaceActGbc.gridx = 3;
+		lowerFaceActGbc.gridy = 1;
+		exprPanel.add(lowerFaceAct, lowerFaceActGbc);
+
+		lowerFaceAct.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent ev) {
+				if (ev.getStateChange() == ItemEvent.SELECTED) {
+					updateLowerFaceAct(true);
+				} else {
+					updateLowerFaceAct(false);
+				}
+			}
+		});
+
+		JLabel upperFaceLbl = new JLabel("Upper Face");
+		GridBagConstraints upperFaceGbc = new GridBagConstraints();
+		upperFaceGbc.gridx = 1;
+		upperFaceGbc.gridy = 2;
+		exprPanel.add(upperFaceLbl, upperFaceGbc);
+
+		JComboBox<Object> upperFaceCombo = new JComboBox<Object>();
+		upperFaceCombo.setModel(new DefaultComboBoxModel<Object>(upperFaceList));
+		GridBagConstraints upperFaceComboGbc = new GridBagConstraints();
+		upperFaceComboGbc.gridx = 2;
+		upperFaceComboGbc.gridy = 2;
+		exprPanel.add(upperFaceCombo, upperFaceComboGbc);
+
+		upperFaceCombo.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+
+				updateUpperFace(String.valueOf(upperFaceCombo.getSelectedItem()));
+			}
+		});
+
+		JToggleButton upperFaceAct = new JToggleButton("Activate");
+		GridBagConstraints upperFaceActGbc = new GridBagConstraints();
+		upperFaceActGbc.gridx = 3;
+		upperFaceActGbc.gridy = 2;
+		exprPanel.add(upperFaceAct, upperFaceActGbc);
+
+		upperFaceAct.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent ev) {
+				if (ev.getStateChange() == ItemEvent.SELECTED) {
+					updateUpperFaceAct(true);
+				} else {
+					updateUpperFaceAct(false);
+				}
+			}
+		});
+
+		JLabel eyeLabel = new JLabel("Eye");
+		GridBagConstraints eyeGbc = new GridBagConstraints();
+		eyeGbc.gridx = 1;
+		eyeGbc.gridy = 3;
+		exprPanel.add(eyeLabel, eyeGbc);
+
+		JComboBox<Object> eyeCombo = new JComboBox<Object>();
+		eyeCombo.setModel(new DefaultComboBoxModel<Object>(eyeList));
+		GridBagConstraints eyeComboGbc = new GridBagConstraints();
+		eyeComboGbc.gridx = 2;
+		eyeComboGbc.gridy = 3;
+		exprPanel.add(eyeCombo, eyeComboGbc);
+
+		eyeCombo.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				updateEye(String.valueOf(eyeCombo.getSelectedItem()));
+			}
+		});
+
+		JToggleButton eyeAct = new JToggleButton("Activate");
+		GridBagConstraints eyeActGbc = new GridBagConstraints();
+		eyeActGbc.gridx = 3;
+		eyeActGbc.gridy = 3;
+		exprPanel.add(eyeAct, eyeActGbc);
+
+		eyeAct.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent ev) {
+				if (ev.getStateChange() == ItemEvent.SELECTED) {
+					updateEyeAct(true);
+				} else {
+					updateEyeAct(false);
+				}
+			}
+		});
+
+		JCheckBox eyeCheckBox = new JCheckBox("Auto Reset");
+		GridBagConstraints eyeCheckBoxGbc = new GridBagConstraints();
+		eyeCheckBoxGbc.gridx = 4;
+		eyeCheckBoxGbc.gridy = 3;
+		exprPanel.add(eyeCheckBox, eyeCheckBoxGbc);
+
+		eyeCheckBox.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				AbstractButton abstractButton = (AbstractButton) e.getSource();
+				updateEyeAutoReset(abstractButton.getModel().isSelected());
+			}
+		});
+
+		splitPane3.setOrientation(JSplitPane.VERTICAL_SPLIT);
+		splitPane3.setDividerLocation(150);
+		splitPane3.setTopComponent(exprPanel);
+		splitPane3.setBottomComponent(consolePanel);
+		splitPane3.setDividerSize(0);
+		splitPane3.setBorder(BorderFactory.createMatteBorder(0, 0, 0, 0, Color.BLACK));
+
+		splitPane2.setOrientation(JSplitPane.VERTICAL_SPLIT);
+		splitPane2.setDividerLocation(150);
+		splitPane2.setTopComponent(emotionsPanel);
+		splitPane2.setBottomComponent(splitPane3);
+		splitPane2.setDividerSize(0);
+		splitPane2.setBorder(BorderFactory.createMatteBorder(0, 0, 0, 0, Color.BLACK));
+
+		splitPane1.setOrientation(JSplitPane.VERTICAL_SPLIT);
+		splitPane1.setDividerLocation(50);
+		splitPane1.setTopComponent(timerPanel);
+		splitPane1.setBottomComponent(splitPane2);
+		splitPane1.setDividerSize(0);
+		splitPane1.setBorder(BorderFactory.createMatteBorder(0, 0, 0, 0, Color.BLACK));
+
+		configPanel.add(splitPane1);
 		return configPanel;
 	}
 
@@ -145,4 +322,31 @@ public class ServerPanelGenerator {
 		ServerCommonData.getInstance().setInterval(Integer.parseInt(intervalInputTextField.getText()));
 	}
 
+	private static void updateLowerFace(String lowerFaceVal) {
+		ServerCommonData.getInstance().setLowerFace(lowerFaceVal);
+	}
+
+	private static void updateUpperFace(String UpperFaceVal) {
+		ServerCommonData.getInstance().setUpperFace(UpperFaceVal);
+	}
+
+	private static void updateEye(String eyeVal) {
+		ServerCommonData.getInstance().setEye(eyeVal);
+	}
+
+	private static void updateLowerFaceAct(Boolean lowerFaceVal) {
+		ServerCommonData.getInstance().setLowerFaceAct(lowerFaceVal);
+	}
+
+	private static void updateUpperFaceAct(Boolean UpperFaceVal) {
+		ServerCommonData.getInstance().setUpperFaceAct(UpperFaceVal);
+	}
+
+	private static void updateEyeAct(Boolean eyeVal) {
+		ServerCommonData.getInstance().setEyeAct(eyeVal);
+	}
+
+	private static void updateEyeAutoReset(Boolean eyeAutoResetVal) {
+		ServerCommonData.getInstance().setEyeAutoReset(eyeAutoResetVal);
+	}
 }
--- src/main/java/ser516/project3/utilities/ServerCommonData.java
+++ src/main/java/ser516/project3/utilities/ServerCommonData.java
@@ -7,9 +7,26 @@ public class ServerCommonData {
 	private static ServerCommonData instance = null;
 	private static Message message;
 	private int interval;
+	private String lowerFace;
+	private String upperFace;
+	private String eye;
+	private Boolean lowerFaceAct;
+	private Boolean upperFaceAct;
+	private Boolean eyeAct;
+	private Boolean eyeAutoReset;
+	
 
 	protected ServerCommonData() {
+		lowerFace = "Smile";
+		upperFace = "Raise Brow";
+		eye = "Blink";
+		lowerFaceAct = false;
+		upperFaceAct = false;
+		eyeAct = false;
+		eyeAutoReset = false;
 	}
+	
+	
 
 	public static ServerCommonData getInstance() {
 		if (instance == null) {
@@ -36,4 +53,117 @@ public class ServerCommonData {
 	public void setInterval(int interval) {
 		this.interval = interval;
 	}
+	
+	/**
+	 * @return the lowerFace Parameter
+	 */
+	public String getLowerFace() {
+		return lowerFace;
+	}
+
+	/**
+	 * @param lowerFace - sets the lowerFace attribute
+	 */
+	public void setLowerFace(String lowerFace) {
+		this.lowerFace = lowerFace;
+	}
+
+	/**
+	 * @return the upperFace Parameter
+	 */
+	public String getUpperFace() {
+		return upperFace;
+	}
+
+	/**
+	 * @param upperFace - sets the upperFace attribute
+	 */
+	public void setUpperFace(String upperFace) {
+		this.upperFace = upperFace;
+	}
+
+	/**
+	 * @return the eye Parameter
+	 */
+	public String getEye() {
+		return eye;
+	}
+
+	/**
+	 * @param eye - sets the eye attribute
+	 */
+	public void setEye(String eye) {
+		this.eye = eye;
+	}
+	
+	
+	/**
+	 * @return the eye Auto Reset Value
+	 */
+	
+	public Boolean getEyeAutoReset() {
+		return eyeAutoReset;
+	}
+
+	
+	/**
+	 * @param eyeAutoReset - sets the eye Auto Reset attribute
+	 */
+	
+	public void setEyeAutoReset(Boolean eyeAutoReset) {
+		this.eyeAutoReset = eyeAutoReset;
+	}
+
+	/**
+	 * @return the Lower Face Activated Button Status
+	 */
+
+	public Boolean getLowerFaceAct() {
+		return lowerFaceAct;
+	}
+
+
+	/**
+	 * @param lowerFaceAct - sets the Lower Face Activated Button Status attribute
+	 */
+	
+	public void setLowerFaceAct(Boolean lowerFaceAct) {
+		this.lowerFaceAct = lowerFaceAct;
+		
+	}
+
+	/**
+	 * @return the Upper Face Activated Button Status
+	 */
+	
+	public Boolean getUpperFaceAct() {
+		return upperFaceAct;
+	}
+
+	/**
+	 * @param upperFaceAct - sets the Upper Face Activated Button Status attribute
+	 */
+	
+	public void setUpperFaceAct(Boolean upperFaceAct) {
+		this.upperFaceAct = upperFaceAct;
+		
+	}
+
+	/**
+	 * @return the Eye Activated Button Status
+	 */
+	
+	public Boolean getEyeAct() {
+		return eyeAct;
+	}
+	
+	/**
+	 * @param eyeAct - sets the Eye Activated Button Status attribute
+	 */
+
+	public void setEyeAct(Boolean eyeAct) {
+		this.eyeAct = eyeAct;
+		
+	}
+
 }
