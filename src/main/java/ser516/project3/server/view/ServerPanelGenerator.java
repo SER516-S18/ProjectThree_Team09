3daacaca917006d35fa0c1320a75d9710db59c19
Manish Tandon <mtandon3@asu.edu>
2018-04-01 14:57:14 -0700

    updated variable names in ServerPanelGenerator

--- src/main/java/ser516/project3/server/view/ServerPanelGenerator.java
+++ src/main/java/ser516/project3/server/view/ServerPanelGenerator.java
@@ -45,6 +45,10 @@ public class ServerPanelGenerator {
 
 	private static final Font FONT = new Font("Courier New", Font.BOLD, 17);
 	private static final Font SUBFONT = new Font("Courier New", Font.BOLD, 14);
+	private static final String INTERVAL_LABEL_NAME="Interval (seconds):  ";
+	private static final String AUTO_REPEAT_CHECKBOX_NAME="Auto Repeat";
+	private static final String TOGGLE_START_STOP="Start / Stop";
+
 
 	/**
 	 * This method will initialize the top JPanels of the server application
@@ -65,7 +69,7 @@ public class ServerPanelGenerator {
 		Border compoundBorder = BorderFactory.createCompoundBorder(marginBorder, titledBorder);
 		topPanel.setBorder(compoundBorder);
 
-		JLabel intervalLabel = new JLabel("Interval (seconds):  ");
+		JLabel intervalLabel = new JLabel(INTERVAL_LABEL_NAME);
 		intervalLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		intervalLabel.setOpaque(true);
 
@@ -95,7 +99,7 @@ public class ServerPanelGenerator {
 		});
 
 
-		JCheckBox autoRepeatCheckBox = new JCheckBox("Auto Repeat", false);
+		JCheckBox autoRepeatCheckBox = new JCheckBox(AUTO_REPEAT_CHECKBOX_NAME, false);
 		autoRepeatCheckBox.setHorizontalAlignment(SwingConstants.CENTER);
 		autoRepeatCheckBox.addActionListener(new ActionListener() {
 
@@ -107,7 +111,7 @@ public class ServerPanelGenerator {
 
 		});
 		
-		JButton buttonToggle = new JButton("Start / Stop");
+		JButton buttonToggle = new JButton(TOGGLE_START_STOP);
 		buttonToggle.setHorizontalAlignment(SwingConstants.CENTER);
 		buttonToggle.addActionListener(new ActionListener() {
 			
@@ -169,7 +173,7 @@ public class ServerPanelGenerator {
 		JPanel configPanel = new JPanel();
 		JPanel timerPanel = new JPanel();
 		JPanel emotionsPanel = new JPanel();
-		JPanel exprPanel = new JPanel();
+		JPanel expressionPanel = new JPanel();
 		JPanel consolePanel = new JPanel();
 
 		JSplitPane splitPane1 = new JSplitPane();
@@ -190,27 +194,27 @@ public class ServerPanelGenerator {
 
 		configPanel.setLayout(new BorderLayout(0, 0));
 
-		exprPanel.setLayout(new GridBagLayout());
+		expressionPanel.setLayout(new GridBagLayout());
 
 		emotionsPanel
 				.setBorder(new TitledBorder(null, "Emotions", TitledBorder.LEADING, TitledBorder.TOP, SUBFONT, null));
-		exprPanel.setBorder(
+		expressionPanel.setBorder(
 				new TitledBorder(null, "Expressions", TitledBorder.LEADING, TitledBorder.TOP, SUBFONT, null));
 		consolePanel
 				.setBorder(new TitledBorder(null, "Console", TitledBorder.LEADING, TitledBorder.TOP, SUBFONT, null));
 
-		JLabel lowerFaceLbl = new JLabel("Lower Face");
-		GridBagConstraints lowerFaceGbc = new GridBagConstraints();
-		lowerFaceGbc.gridx = 1;
-		lowerFaceGbc.gridy = 1;
-		exprPanel.add(lowerFaceLbl, lowerFaceGbc);
+		JLabel lowerFaceLabel = new JLabel("Lower Face");
+		GridBagConstraints lowerFaceGridConstraints = new GridBagConstraints();
+		lowerFaceGridConstraints.gridx = 1;
+		lowerFaceGridConstraints.gridy = 1;
+		expressionPanel.add(lowerFaceLabel, lowerFaceGridConstraints);
 
 		JComboBox<Object> lowerFaceCombo = new JComboBox<Object>();
 		lowerFaceCombo.setModel(new DefaultComboBoxModel<Object>(lowerFaceList));
 		GridBagConstraints lowerFaceComboGbc = new GridBagConstraints();
 		lowerFaceComboGbc.gridx = 2;
 		lowerFaceComboGbc.gridy = 1;
-		exprPanel.add(lowerFaceCombo, lowerFaceComboGbc);
+		expressionPanel.add(lowerFaceCombo, lowerFaceComboGbc);
 
 		lowerFaceCombo.addActionListener(new ActionListener() {
 
@@ -219,13 +223,13 @@ public class ServerPanelGenerator {
 			}
 		});
 
-		JToggleButton lowerFaceAct = new JToggleButton("Activate");
-		GridBagConstraints lowerFaceActGbc = new GridBagConstraints();
-		lowerFaceActGbc.gridx = 3;
-		lowerFaceActGbc.gridy = 1;
-		exprPanel.add(lowerFaceAct, lowerFaceActGbc);
+		JToggleButton lowerFaceActivateButton = new JToggleButton("Activate");
+		GridBagConstraints lowerFaceActivateButtonConstraints = new GridBagConstraints();
+		lowerFaceActivateButtonConstraints.gridx = 3;
+		lowerFaceActivateButtonConstraints.gridy = 1;
+		expressionPanel.add(lowerFaceActivateButton, lowerFaceActivateButtonConstraints);
 
-		lowerFaceAct.addItemListener(new ItemListener() {
+		lowerFaceActivateButton.addItemListener(new ItemListener() {
 			public void itemStateChanged(ItemEvent ev) {
 				if (ev.getStateChange() == ItemEvent.SELECTED) {
 					updateLowerFaceAct(true);
@@ -239,14 +243,14 @@ public class ServerPanelGenerator {
 		GridBagConstraints upperFaceGbc = new GridBagConstraints();
 		upperFaceGbc.gridx = 1;
 		upperFaceGbc.gridy = 2;
-		exprPanel.add(upperFaceLbl, upperFaceGbc);
+		expressionPanel.add(upperFaceLbl, upperFaceGbc);
 
 		JComboBox<Object> upperFaceCombo = new JComboBox<Object>();
 		upperFaceCombo.setModel(new DefaultComboBoxModel<Object>(upperFaceList));
-		GridBagConstraints upperFaceComboGbc = new GridBagConstraints();
-		upperFaceComboGbc.gridx = 2;
-		upperFaceComboGbc.gridy = 2;
-		exprPanel.add(upperFaceCombo, upperFaceComboGbc);
+		GridBagConstraints upperFaceComboConstraints = new GridBagConstraints();
+		upperFaceComboConstraints.gridx = 2;
+		upperFaceComboConstraints.gridy = 2;
+		expressionPanel.add(upperFaceCombo, upperFaceComboConstraints);
 
 		upperFaceCombo.addActionListener(new ActionListener() {
 
@@ -260,7 +264,7 @@ public class ServerPanelGenerator {
 		GridBagConstraints upperFaceActGbc = new GridBagConstraints();
 		upperFaceActGbc.gridx = 3;
 		upperFaceActGbc.gridy = 2;
-		exprPanel.add(upperFaceAct, upperFaceActGbc);
+		expressionPanel.add(upperFaceAct, upperFaceActGbc);
 
 		upperFaceAct.addItemListener(new ItemListener() {
 			public void itemStateChanged(ItemEvent ev) {
@@ -276,14 +280,14 @@ public class ServerPanelGenerator {
 		GridBagConstraints eyeGbc = new GridBagConstraints();
 		eyeGbc.gridx = 1;
 		eyeGbc.gridy = 3;
-		exprPanel.add(eyeLabel, eyeGbc);
+		expressionPanel.add(eyeLabel, eyeGbc);
 
 		JComboBox<Object> eyeCombo = new JComboBox<Object>();
 		eyeCombo.setModel(new DefaultComboBoxModel<Object>(eyeList));
-		GridBagConstraints eyeComboGbc = new GridBagConstraints();
-		eyeComboGbc.gridx = 2;
-		eyeComboGbc.gridy = 3;
-		exprPanel.add(eyeCombo, eyeComboGbc);
+		GridBagConstraints eyeComboConstraints = new GridBagConstraints();
+		eyeComboConstraints.gridx = 2;
+		eyeComboConstraints.gridy = 3;
+		expressionPanel.add(eyeCombo, eyeComboConstraints);
 
 		eyeCombo.addActionListener(new ActionListener() {
 
@@ -292,13 +296,13 @@ public class ServerPanelGenerator {
 			}
 		});
 
-		JToggleButton eyeAct = new JToggleButton("Activate");
-		GridBagConstraints eyeActGbc = new GridBagConstraints();
-		eyeActGbc.gridx = 3;
-		eyeActGbc.gridy = 3;
-		exprPanel.add(eyeAct, eyeActGbc);
+		JToggleButton eyeActivateButton = new JToggleButton("Activate");
+		GridBagConstraints eyeActConstraints = new GridBagConstraints();
+		eyeActConstraints.gridx = 3;
+		eyeActConstraints.gridy = 3;
+		expressionPanel.add(eyeActivateButton, eyeActConstraints);
 
-		eyeAct.addItemListener(new ItemListener() {
+		eyeActivateButton.addItemListener(new ItemListener() {
 			public void itemStateChanged(ItemEvent ev) {
 				if (ev.getStateChange() == ItemEvent.SELECTED) {
 					updateEyeAct(true);
@@ -309,10 +313,10 @@ public class ServerPanelGenerator {
 		});
 
 		JCheckBox eyeCheckBox = new JCheckBox("Auto Reset");
-		GridBagConstraints eyeCheckBoxGbc = new GridBagConstraints();
-		eyeCheckBoxGbc.gridx = 4;
-		eyeCheckBoxGbc.gridy = 3;
-		exprPanel.add(eyeCheckBox, eyeCheckBoxGbc);
+		GridBagConstraints eyeCheckBoxConstraints = new GridBagConstraints();
+		eyeCheckBoxConstraints.gridx = 4;
+		eyeCheckBoxConstraints.gridy = 3;
+		expressionPanel.add(eyeCheckBox, eyeCheckBoxConstraints);
 
 		eyeCheckBox.addActionListener(new ActionListener() {
 
@@ -324,7 +328,7 @@ public class ServerPanelGenerator {
 
 		splitPane3.setOrientation(JSplitPane.VERTICAL_SPLIT);
 		splitPane3.setDividerLocation(150);
-		splitPane3.setTopComponent(exprPanel);
+		splitPane3.setTopComponent(expressionPanel);
 		splitPane3.setBottomComponent(consolePanel);
 		splitPane3.setDividerSize(0);
 		splitPane3.setBorder(BorderFactory.createMatteBorder(0, 0, 0, 0, Color.BLACK));
@@ -356,24 +360,24 @@ public class ServerPanelGenerator {
 		}
 	}
 
-	private static void updateLowerFace(String lowerFaceVal) {
-		ServerCommonData.getInstance().setLowerFace(lowerFaceVal);
+	private static void updateLowerFace(String lowerFaceValue) {
+		ServerCommonData.getInstance().setLowerFace(lowerFaceValue);
 	}
 
-	private static void updateUpperFace(String UpperFaceVal) {
-		ServerCommonData.getInstance().setUpperFace(UpperFaceVal);
+	private static void updateUpperFace(String upperFaceValue) {
+		ServerCommonData.getInstance().setUpperFace(upperFaceValue);
 	}
 
-	private static void updateEye(String eyeVal) {
-		ServerCommonData.getInstance().setEye(eyeVal);
+	private static void updateEye(String eyeValue) {
+		ServerCommonData.getInstance().setEye(eyeValue);
 	}
 
-	private static void updateLowerFaceAct(Boolean lowerFaceVal) {
-		ServerCommonData.getInstance().setLowerFaceAct(lowerFaceVal);
+	private static void updateLowerFaceAct(Boolean lowerFaceValue) {
+		ServerCommonData.getInstance().setLowerFaceAct(lowerFaceValue);
 	}
 
-	private static void updateUpperFaceAct(Boolean UpperFaceVal) {
-		ServerCommonData.getInstance().setUpperFaceAct(UpperFaceVal);
+	private static void updateUpperFaceAct(Boolean upperFaceValue) {
+		ServerCommonData.getInstance().setUpperFaceAct(upperFaceValue);
 	}
 
 	private static void updateEyeAct(Boolean eyeVal) {
