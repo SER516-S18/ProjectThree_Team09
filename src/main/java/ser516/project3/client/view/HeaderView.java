505a23b88cfd38a6e2b6e0bca3affebba01137cd
Adhiraj Tikku <adhiraj.192@gmail.com>
2018-04-01 18:36:15 -0700

    refactoring header view

--- src/main/java/ser516/project3/client/view/HeaderView.java
+++ src/main/java/ser516/project3/client/view/HeaderView.java
@@ -11,36 +11,41 @@ import ser516.project3.server.view.*;
  * HeaderView class to implement the header view for client to show and update
  * the client status
  * 
- * @author Vishakha Singal
+ * @author Vishakha Singal, Adhiraj Tikku
  * @version 1.0
  */
-
-@SuppressWarnings("serial")
 public class HeaderView extends JPanel {
 
 	private JLabel connectionLabel;
 	private JLabel timeStampLabel;
+	private JLabel connectionTextLabel;
+	private JLabel timestampTextLabel;
 	private JButton connectButton;
 	private JButton serverButton;
 	private JDialog serverDialog;
 
 	public HeaderView() {
-		this.setLayout(new BorderLayout());
-		this.setOpaque(false);
-		this.setBorder(new EmptyBorder(8, 8, 8, 8));
-		this.setLayout(new GridBagLayout());
-		this.setBackground(Color.WHITE);
+		setLayout(new BorderLayout());
+		setOpaque(false);
+		setBorder(new EmptyBorder(8, 8, 8, 8));
+		setLayout(new GridBagLayout());
+		setBackground(Color.WHITE);
 
 		GridBagConstraints bagConstraints = new GridBagConstraints();
 		bagConstraints.fill = GridBagConstraints.HORIZONTAL;
 
-		JLabel connectionTextLabel = new JLabel("Status:");
+		createLabels(bagConstraints);
+		createButtons(bagConstraints);
+	}
+
+	private void createLabels(GridBagConstraints bagConstraints) {
+		connectionTextLabel = new JLabel("Status:");
 		connectionTextLabel.setHorizontalAlignment(JLabel.RIGHT);
 		connectionTextLabel.setVerticalTextPosition(JLabel.CENTER);
 		bagConstraints.gridx = 0;
 		bagConstraints.gridy = 0;
 		bagConstraints.insets = new Insets(0, 0, 10, 10);
-		this.add(connectionTextLabel, bagConstraints);
+		add(connectionTextLabel, bagConstraints);
 
 		connectionLabel = new JLabel("Not Connected");
 		connectionLabel.setHorizontalTextPosition(JLabel.LEFT);
@@ -48,8 +53,26 @@ public class HeaderView extends JPanel {
 		bagConstraints.gridx = 1;
 		bagConstraints.gridy = 0;
 		bagConstraints.insets = new Insets(0, 0, 10, 10);
-		this.add(connectionLabel, bagConstraints);
+		add(connectionLabel, bagConstraints);
 
+		timestampTextLabel = new JLabel("Time Stamp: ");
+		timestampTextLabel.setHorizontalAlignment(JLabel.RIGHT);
+		timestampTextLabel.setVerticalTextPosition(JLabel.CENTER);
+		bagConstraints.gridx = 0;
+		bagConstraints.gridy = 1;
+		bagConstraints.insets = new Insets(0, 0, 0, 10);
+		add(timestampTextLabel, bagConstraints);
+
+		timeStampLabel = new JLabel("0.0");
+		timeStampLabel.setHorizontalTextPosition(JLabel.LEFT);
+		timeStampLabel.setVerticalTextPosition(JLabel.CENTER);
+		bagConstraints.gridx = 1;
+		bagConstraints.gridy = 1;
+		bagConstraints.insets = new Insets(0, 0, 0, 10);
+		add(timeStampLabel, bagConstraints);
+	}
+
+	private void createButtons(GridBagConstraints bagConstraints) {
 		connectButton = new JButton("Connect");
 		connectButton.setBackground(Color.RED);
 		connectButton.setPreferredSize(new Dimension(120, 35));
@@ -57,7 +80,7 @@ public class HeaderView extends JPanel {
 		bagConstraints.gridy = 0;
 		bagConstraints.gridheight = 3;
 		bagConstraints.insets = new Insets(0, 20, 0, 20);
-		this.add(connectButton, bagConstraints);
+		add(connectButton, bagConstraints);
 
 		serverButton = new JButton("Open Server");
 		serverButton.setBackground(Color.BLUE);
@@ -66,7 +89,7 @@ public class HeaderView extends JPanel {
 		bagConstraints.gridy = -1;
 		bagConstraints.gridheight = 2;
 		bagConstraints.insets = new Insets(7, 5, 5, 0);
-		this.add(serverButton, bagConstraints);
+		add(serverButton, bagConstraints);
 		serverButton.addActionListener(new ActionListener() {
 
 			@Override
@@ -79,23 +102,6 @@ public class HeaderView extends JPanel {
 			}
 
 		});
-
-		JLabel timestampTextLabel = new JLabel("Time Stamp: ");
-		timestampTextLabel.setHorizontalAlignment(JLabel.RIGHT);
-		timestampTextLabel.setVerticalTextPosition(JLabel.CENTER);
-		bagConstraints.gridx = 0;
-		bagConstraints.gridy = 1;
-		bagConstraints.insets = new Insets(0, 0, 0, 10);
-		this.add(timestampTextLabel, bagConstraints);
-
-		timeStampLabel = new JLabel("0.0");
-		timeStampLabel.setHorizontalTextPosition(JLabel.LEFT);
-		timeStampLabel.setVerticalTextPosition(JLabel.CENTER);
-		bagConstraints.gridx = 1;
-		bagConstraints.gridy = 1;
-		bagConstraints.insets = new Insets(0, 0, 0, 10);
-		this.add(timeStampLabel, bagConstraints);
-
 	}
 
 	public void addConnectButtonListener(ActionListener actionListener) {
