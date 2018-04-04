eee91fb5673e8afa25a286a5119d2ff99221d809
svganesh93 <svganesh93@gmail.com>
2018-04-03 21:11:08 -0700

    Update ServerPanelGenerator.java

--- src/main/java/ser516/project3/server/view/ServerPanelGenerator.java
+++ src/main/java/ser516/project3/server/view/ServerPanelGenerator.java
@@ -3,22 +3,16 @@ package ser516.project3.server.view;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 
 import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.border.TitledBorder;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 
 import org.apache.log4j.Logger;
 
 import ser516.project3.model.ConsoleModel;
-import ser516.project3.model.Message.AbstractExpression;
-import ser516.project3.model.Message.ConcreteExpression;
 import ser516.project3.server.controller.ServerControllerImpl;
 import ser516.project3.utilities.InputVerifierNumericals;
 import ser516.project3.utilities.ServerCommonData;
@@ -30,7 +24,7 @@ public class ServerPanelGenerator {
 	private static ServerControllerImpl serverControllerImpl = new ServerControllerImpl();
 
 	private static final Font FONT = new Font("Courier New", Font.BOLD, 17);
-	private static final Font SUBFONT = new Font("Courier New", Font.BOLD, 14);
+	//private static final Font SUBFONT = new Font("Courier New", Font.BOLD, 14);
 	private static final String INTERVAL_LABEL_NAME = "Interval (seconds):  ";
 	private static final String AUTO_REPEAT_CHECKBOX_NAME = "Auto Repeat";
 	private static final String TOGGLE_START_STOP = "Start / Stop";
@@ -206,197 +200,14 @@ public class ServerPanelGenerator {
 	}
 
 	public static JPanel createExpressionPanel() {
-		JPanel expressionPanel = new JPanel();
-		String[] lowerFaceList = { "Smile", "Clench", "Smirk Left", "Smirk Right", "Laugh" };
-		String[] upperFaceList = { "Raise Brow", "Furrow Brow" };
-		String[] eyeList = { "Blink", "Wink Left", "Wink Right", "Look Left", "Look Right" };
-		Dimension spinnerDimension = new Dimension(60, 30);
-		JLabel lowerFaceLbl = new JLabel("Lower Face");
-
-		expressionPanel.setLayout(new GridBagLayout());
-		expressionPanel.setBorder(
-				new TitledBorder(null, "Expressions", TitledBorder.LEADING, TitledBorder.TOP, SUBFONT, null));
-
-		GridBagConstraints lowerFaceGbc = new GridBagConstraints();
-		lowerFaceGbc.gridx = 1;
-		lowerFaceGbc.gridy = 1;
-		expressionPanel.add(lowerFaceLbl, lowerFaceGbc);
-
-		JComboBox<Object> lowerFaceCombo = new JComboBox<Object>();
-		lowerFaceCombo.setModel(new DefaultComboBoxModel<Object>(lowerFaceList));
-		lowerFaceCombo.setPreferredSize(new Dimension(120, 30));
-		GridBagConstraints lowerFaceComboGbc = new GridBagConstraints();
-		lowerFaceComboGbc.gridx = 2;
-		lowerFaceComboGbc.gridy = 1;
-		expressionPanel.add(lowerFaceCombo, lowerFaceComboGbc);
-
-		SpinnerModel lowerFaceModel = new SpinnerNumberModel(0.0, 0.0, 1.0, 0.1);
-		JSpinner lowerFaceSpinner = new JSpinner(lowerFaceModel);
-
-		lowerFaceSpinner.setPreferredSize(spinnerDimension);
-		GridBagConstraints lowerFaceActGbc = new GridBagConstraints();
-		lowerFaceActGbc.gridx = 3;
-		lowerFaceActGbc.gridy = 1;
-		expressionPanel.add(lowerFaceSpinner, lowerFaceActGbc);
-
-		JLabel upperFaceLbl = new JLabel("Upper Face");
-		GridBagConstraints upperFaceGbc = new GridBagConstraints();
-		upperFaceGbc.gridx = 1;
-		upperFaceGbc.gridy = 2;
-		expressionPanel.add(upperFaceLbl, upperFaceGbc);
-
-		JComboBox<Object> upperFaceCombo = new JComboBox<Object>();
-		upperFaceCombo.setModel(new DefaultComboBoxModel<Object>(upperFaceList));
-		upperFaceCombo.setPreferredSize(new Dimension(120, 30));
-
-		GridBagConstraints upperFaceComboGbc = new GridBagConstraints();
-		upperFaceComboGbc.gridx = 2;
-		upperFaceComboGbc.gridy = 2;
-		expressionPanel.add(upperFaceCombo, upperFaceComboGbc);
-
-		SpinnerModel upperFaceModel = new SpinnerNumberModel(0.00, 0.00, 1.00, 0.10);
-		JSpinner upperFaceSpinner = new JSpinner(upperFaceModel);
-
-		upperFaceSpinner.setPreferredSize(spinnerDimension);
-		GridBagConstraints upperFaceActGbc = new GridBagConstraints();
-		upperFaceActGbc.gridx = 3;
-		upperFaceActGbc.gridy = 2;
-		expressionPanel.add(upperFaceSpinner, upperFaceActGbc);
-
-		JLabel eyeLabel = new JLabel("Eye");
-		GridBagConstraints eyeGbc = new GridBagConstraints();
-		eyeGbc.gridx = 1;
-		eyeGbc.gridy = 3;
-		expressionPanel.add(eyeLabel, eyeGbc);
-
-		JComboBox<Object> eyeCombo = new JComboBox<Object>();
-		eyeCombo.setModel(new DefaultComboBoxModel<Object>(eyeList));
-		eyeCombo.setPreferredSize(new Dimension(120, 30));
-
-		GridBagConstraints eyeComboGbc = new GridBagConstraints();
-		eyeComboGbc.gridx = 2;
-		eyeComboGbc.gridy = 3;
-		expressionPanel.add(eyeCombo, eyeComboGbc);
-
-		JToggleButton eyeAct = new JToggleButton("Activate");
-		eyeAct.setPreferredSize(new Dimension(90, 30));
-		GridBagConstraints eyeActGbc = new GridBagConstraints();
-		eyeActGbc.gridx = 3;
-		eyeActGbc.gridy = 3;
-		expressionPanel.add(eyeAct, eyeActGbc);
-
-		JCheckBox eyeCheckBox = new JCheckBox("Auto Reset");
-		GridBagConstraints eyeCheckBoxGbc = new GridBagConstraints();
-		eyeCheckBoxGbc.gridx = 4;
-		eyeCheckBoxGbc.gridy = 3;
-		expressionPanel.add(eyeCheckBox, eyeCheckBoxGbc);
-
-		lowerFaceCombo.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				String attribute = String.valueOf(lowerFaceCombo.getSelectedItem());
-				Double value = (Double) lowerFaceSpinner.getValue();
-				updateLowerFace(attribute, value);
-			}
-		});
-
-		lowerFaceSpinner.addChangeListener(new ChangeListener() {
-			@Override
-			public void stateChanged(ChangeEvent e) {
-				String attribute = String.valueOf(lowerFaceCombo.getSelectedItem());
-				Double value = (Double) lowerFaceSpinner.getValue();
-				updateLowerFace(attribute, value);
-			}
-		});
-
-		upperFaceCombo.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				String attribute = String.valueOf(upperFaceCombo.getSelectedItem());
-				Double value = (Double) upperFaceSpinner.getValue();
-				updateUpperFace(attribute, value);
-			}
-		});
-
-		upperFaceSpinner.addChangeListener(new ChangeListener() {
-			@Override
-			public void stateChanged(ChangeEvent e) {
-				String attribute = String.valueOf(upperFaceCombo.getSelectedItem());
-				Double value = (Double) upperFaceSpinner.getValue();
-				updateUpperFace(attribute, value);
-			}
-		});
-
-		eyeCombo.addActionListener(new ActionListener() {
-
-			public void actionPerformed(ActionEvent e) {
-				String attribute = String.valueOf(eyeCombo.getSelectedItem());
-				Boolean eyeStatus = eyeAct.isSelected();
-				updateEye(attribute, eyeStatus);
-			}
-		});
-
-		eyeAct.addItemListener(new ItemListener() {
-			public void itemStateChanged(ItemEvent ev) {
-				String attribute = String.valueOf(eyeCombo.getSelectedItem());
-				Boolean eyeStatus = eyeAct.isSelected();
-				updateEye(attribute, eyeStatus);
-			}
-		});
-
-		eyeCheckBox.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				// To be implemented
-				// AbstractButton abstractButton = (AbstractButton) e.getSource();
-				// updateEyeAutoReset(abstractButton.getModel().isSelected());
-
-			}
-		});
-
-		return expressionPanel;
-	}
-
-	public enum EmotionPanel{
-		Interest(1, 1, 3, 1), Engagement(1, 2, 3, 2), Excitement(1, 3, 3, 3), Stress(2, 2, 3, 4), Relaxation(1, 5, 3, 5), Focus(1, 6, 3, 6);
-		int gbc_x, gbc_y, spinner_x, spinner_y;
-		private EmotionPanel(int gbc_x, int gbc_y, int spinner_x, int spinner_y){
-			this.gbc_x = gbc_x;
-			this.gbc_y = gbc_y;
-			this.spinner_x = spinner_x;
-			this.spinner_y = spinner_y;
-		}
+		ExpressionsView expressionsView = new ExpressionsView();
+		return expressionsView.getExpressionsPanel();
 	}
 	
 	public static JPanel createEmotionsPanel() {
-		JPanel emotionsPanel = new JPanel();
-		emotionsPanel
-				.setBorder(new TitledBorder(null, "Emotions", TitledBorder.LEADING, TitledBorder.TOP, SUBFONT, null));
-
-		Dimension spinnerDimension = new Dimension(65, 30);
-		Double current = new Double(0.00);
-		Double min = new Double(0.00);
-		Double max = new Double(1.00);
-		Double step = new Double(0.10);
-
-		for(EmotionPanel em : EmotionPanel.values()) {
-			JLabel interest_label = new JLabel(em.name());
-			GridBagConstraints gbc = new GridBagConstraints();
-			gbc.gridx = em.gbc_x;
-			gbc.gridy = em.gbc_y;
-			emotionsPanel.add(interest_label, gbc);
-			SpinnerModel spinner = new SpinnerNumberModel(current, min, max, step);
-			JSpinner jspinner = new JSpinner(spinner);
-			jspinner.setPreferredSize(spinnerDimension);
-			GridBagConstraints spinnerGbc = new GridBagConstraints();
-			spinnerGbc.gridx = em.spinner_x;
-			spinnerGbc.gridy = em.spinner_y;
-			emotionsPanel.add(jspinner, spinnerGbc);
-			jspinner.addChangeListener(new ChangeListener() {
-				@Override
-				public void stateChanged(ChangeEvent e) {
-					ServerCommonData.getInstance().getMessage().setEmotion(em.name(), (Double) jspinner.getValue());
-				}
-			});
-		}
-		return emotionsPanel;
+		EmotionsView emotionsView = new EmotionsView();
+		return emotionsView.getEmotionsPanel();
+		
 	}
 
 	public static JPanel createConsolePanel() {
