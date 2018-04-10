e6060a1c72c2db347db2dce4714e145bbdef934d
Manish Tandon <mtandon3@asu.edu>
2018-04-10 12:51:51 -0700

    Comments for face interface

--- src/main/java/ser516/project3/client/view/FaceElementsInterface.java
+++ src/main/java/ser516/project3/client/view/FaceElementsInterface.java
@@ -2,9 +2,39 @@ package ser516.project3.client.view;
 
 import java.awt.Shape;
 
+/**
+ * 
+ * Contract definition for all face elements
+ * 
+ * @author Manish Tandon
+ *
+ */
 public interface FaceElementsInterface extends Shape {
+	/**
+	 * Resets to default position of the face element
+	 */
 	void resetPositionToDefault();
+
+	/**
+	 * Move to a different position as provided by the base class that is implementing this interface
+	 */
 	void moveToDifferentPosition();
-	void moveElement(String instruction,double value);
-	void moveElement(String instruction,boolean value);
+
+	/**
+	 * Moves Face element based on a instruction(can be smile, laugh etc.) and based
+	 * on change value (double) provided by server
+	 * 
+	 * @param instruction
+	 * @param changeValue
+	 */
+	void moveElement(String instruction, double changeValue);
+
+	/**
+	 * Moves Face element based on a instruction(can be smile, laugh etc.) and based
+	 * on change value (boolean) provided by server
+	 * 
+	 * @param instruction
+	 * @param changeValue
+	 */
+	void moveElement(String instruction, boolean changeValue);
 }
